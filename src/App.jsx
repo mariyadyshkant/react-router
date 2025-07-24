c0fcf07c06545e352bb057a6bf466aefb4bc0bce
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-07-24 21:05:41 +0200

    add html for header tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import './index.css'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
 
 
   return (
     <>
-      bs
+      <header>
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+          <a className="navbar-brand fs-5" href="#">Click&Buy</a>
+          <div className="collapse navbar-collapse" id="collapsibleNavId">
+            <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" href="#" aria-current="page">
+                  Homepage
+                  <span className="visually-hidden">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Chi siamo</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Prodotti</a>
+              </li>
+            </ul>
+          </div>
+          <form className="d-flex my-2 my-lg-0">
+            <input
+              className="form-control me-sm-2"
+              type="text"
+              placeholder="Search"
+            />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </nav>
+      </header>
     </>
   )
 }
