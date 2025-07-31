2859334bb76e60444fa90fd8fccce81e746f4e3c
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-07-31 23:52:55 +0200

    add jumbotron and carousel components with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './index.css'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+import NewProducts from './components/NewProducts'
 
 function App() {
 
@@ -39,6 +42,19 @@ function App() {
           </form>
         </nav>
       </header>
+      <main>
+        <div className="jumbotron p-5 text-center">
+          <h1>Welcome to Click&Buy</h1>
+          <p>Your one-stop shop for all your needs.</p>
+          <button type="button" className="btn btn-light btn-lg mt-5">
+            Esplora ora
+          </button>
+        </div>
+        <NewProducts />
+
+
+
+      </main >
     </>
   )
 }
